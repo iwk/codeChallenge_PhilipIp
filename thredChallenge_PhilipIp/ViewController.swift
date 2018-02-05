578b1ba36416927b4ba71e99ecf081e908afe83d
Philip Ip <philip@drifter.net>
2018-02-05 16:40:25 +1100

    Initial Commit

---
+++ thredChallenge_PhilipIp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thredChallenge_PhilipIp
+//
+//  Created by Philip Ip on 5/2/18.
+//  Copyright © 2018 Philip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
